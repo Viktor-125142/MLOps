79006107589ec01e37eecb90cf09a43a74e43daf
Viktor Ilin <viktor125142@gmail.com>
2024-05-06 17:17:37 +0300

    add dvc hw_4

--- HW_4/model_preparation.py
+++ HW_4/model_preparation.py
@@ -3,31 +3,36 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error as mse
 import joblib
+import hydra
+from omegaconf import DictConfig
+import logging
+
+log = logging.getLogger(__name__)
 
 from pipeline import preprocessors_all
 from metric.metric_mse_r2 import calculate_metric
 
-
-def model_preparation():
+@hydra.main(version_base=None, config_path='config', config_name='config')
+def model_preparation(cfg: DictConfig):
     X_train = pd.read_csv("test/X_train.csv")
     y_train = pd.read_csv("test/y_train.csv").squeeze()
 
     pipe_all = Pipeline([
         ('preprocessors', preprocessors_all),
-        ('model', DecisionTreeRegressor(max_leaf_nodes=40,
-                                        min_samples_split=10,
-                                        criterion='absolute_error'
-                                        )
-         )
+        ('model', DecisionTreeRegressor(
+            max_leaf_nodes=cfg.model.params.max_leaf_nodes,
+            min_samples_split=cfg.model.params.min_samples_split,
+            criterion=cfg.model.params.criterion
+        ))
     ])
 
     pipe_all.fit(X_train, y_train)
 
     joblib.dump(pipe_all, 'model/DecisionTreeRegressor_model.pkl')
 
-    print(f"r2 на тренировочной выборке: {calculate_metric(pipe_all, X_train, y_train):.4f}")
+    log.info(f"r2 на тренировочной выборке: {calculate_metric(pipe_all, X_train, y_train):.4f}")
+    log.info(f"mse на тренировочной выборке: {calculate_metric(pipe_all, X_train, y_train, mse):.4f}")
 
-    print(f"mse на тренировочной выборке: {calculate_metric(pipe_all, X_train, y_train, mse):.4f}")
 
-
-model_preparation()
+if __name__ == "__main__":
+    model_preparation()
